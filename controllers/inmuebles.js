509881d99ac2e2dab763498836c8c9d4ff78379e
Antonio <afcoronado1@gmail.com>
2024-05-04 06:28:18 -0500

    Modificando Inmueble

--- controllers/inmuebles.js
+++ controllers/inmuebles.js
@@ -1,15 +1,36 @@
 const { response, request } = require('express');
 const Inmueble = require('../models/inmueble');
+const Villa = require('../models/inmueble');
+const Local = require('../models/inmueble');
 
 const inmueblePost = async(req, res = response) =>{
     const { descripcion, direccion, superficie, tipo,
-         estado,precio,nom_propietario,tel_propietario,zona_ciudad} =req.body;
-    const inmueble = new Inmueble({descripcion, direccion, superficie, tipo,
-        estado,precio,nom_propietario,tel_propietario,zona_ciudad});
-        await inmueble.save();
+         estado,tamaño,urbanizacion,habitaciones,baños,cocinas,puerta_blindada,
+         parqueadero,num_puertas,diafono,acondicionado,
+         precio,nom_propietario,tel_propietario,zona_ciudad} =req.body;
+    // const inmueble = new Inmueble({descripcion, direccion, superficie, tipo,
+    //     estado,precio,nom_propietario,tel_propietario,zona_ciudad});
+    if(tipo=="VILLA"){
+    const villa = new Villa({descripcion, direccion, superficie,tamaño,urbanizacion,
+        habitaciones,baños,cocinas,puerta_blindada,
+        parqueadero,estado,precio,nom_propietario,tel_propietario});
+        
+        await villa.save();
         res.json({
-            inmueble
+            villa
         });
+    }
+    if(tipo=="LOCAL"){
+        const local = new Local({descripcion, direccion,puerta_blindada,
+            zona_ciudad,num_puertas,diafono,acondicionado,
+            estado,precio,nom_propietario,tel_propietario});
+            
+            await local.save();
+            res.json({
+                local
+            });
+    }
+    
 }
 const inmuebleGet = async(req = request, res = response) => {
     const { limite = 5, desde = 0}=req.query;
--- models/inmueble.js
+++ models/inmueble.js
@@ -1,6 +1,7 @@
 
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
 
+const options = { discriminatorKey: 'tipo' };
 const InmuebleSchema = Schema({
     descripcion:{
         type: String,
@@ -13,19 +14,18 @@ const InmuebleSchema = Schema({
         type:Number,
         required:[true, 'La superficie es obligatoria']
     },
-    tipo:{
-        type:String,
-        required:true,
-        enum:['PISO NUEVO','PISO DE OCASION','VILLA','CASA','LOCAL']
-    },
+    // tipo:{
+    //     type:String,
+    //     required:true,
+    //     enum:['PISO NUEVO','PISO DE OCASION','VILLA','CASA','LOCAL']
+    // },
     estado:{
         type:String,
         required:true,
         enum:['VENTA','ALQUILER','VENTA O ALQUILER']
     },
     precio:{
-        type:Number,
-        required:true
+        type:Number
     },
     nom_propietario:{
         type:String,
@@ -35,16 +35,134 @@ const InmuebleSchema = Schema({
         type:String,
         required:true
     },
-    zona_ciudad:{
-        type:String,
-        required:true
-    }
-});
-
-InmuebleSchema.methods.toJSON = function() {
-    const { __v, ...inmueble  } = this.toObject();
     
-    return inmueble;
-};
+},options);
+
+// const VillaInmueble = InmuebleSchema.discriminator(
+//     'Villa', Schema({
+//         tamaño:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },urbanizacion:{
+//             type: String,
+//             required:[true, 'La direccion es obligatoria']
+//         },habitaciones:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },baños:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },cocinas:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },puerta_blindada:{
+//             type: Boolean,
+//             default:true
+//         },parqueadero:{
+//             type: Boolean,
+//             default:true
+//         } 
+//     })
+// );
+
+// const CasaInmueble = InmuebleSchema.discriminator(
+//     'Casa', Schema({
+//         habitaciones:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },baños:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },cocinas:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },puerta_blindada:{
+//             type: Boolean,
+//             default:true
+//         },parqueadero:{
+//             type: Boolean,
+//             default:true
+//         },zona_ciudad:{
+//         type:String,
+//         required:true
+//         }
+//     })
+// );
+// const PisoNuevoInmueble = InmuebleSchema.discriminator(
+//     'Piso Nuevo', Schema({
+//         habitaciones:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },baños:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },cocinas:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },puerta_blindada:{
+//             type: Boolean,
+//             default:true
+//         },parqueadero:{
+//             type: Boolean,
+//             default:true
+//         },zona_ciudad:{
+//             type:String,
+//             required:true
+//         }
+//     })
+// );
+// const PisoOcasionInmueble = InmuebleSchema.discriminator(
+//     'Piso de Ocasión', Schema({
+//         habitaciones:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },baños:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },cocinas:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },puerta_blindada:{
+//             type: Boolean,
+//             default:true
+//         },parqueadero:{
+//             type: Boolean,
+//             default:true
+//         },zona_ciudad:{
+//             type:String,
+//             required:true
+//             }
+//     })
+// );
+// const LocalInmueble = InmuebleSchema.discriminator(
+//     'Local', Schema({
+//         num_puertas:{
+//             type:Number,
+//             required:[true, 'La superficie es obligatoria']
+//         },puerta_blindada:{
+//             type: Boolean,
+//             default:true
+//         },diafono:{
+//             type: Boolean,
+//             default:true
+//         },acondicionado:{
+//             type: Boolean,
+//             default:true
+//         },zona_ciudad:{
+//             type:String,
+//             required:true
+//             }
+//     })
+// );
+// InmuebleSchema.methods.toJSON = function() {
+//     const { __v, ...inmueble  } = this.toObject();
+    
+//     return inmueble;
+// };
 
 module.exports = model('Inmueble', InmuebleSchema);
+// module.exports = model('Villa', VillaInmueble);
+// module.exports = model('Casa', CasaInmueble);
+// module.exports = model('Piso_nuevo', PisoNuevoInmueble);
+// module.exports = model('Piso_Ocasion', PisoOcasionInmueble);
+// module.exports = model('Local', LocalInmueble);
